6c05b2c0c93ddcb5abc10c7832692c9534f134cc
Alsaad Ahamed <alsaadahamed4@gmail.com>
2020-08-07 16:21:38 +0530

    Converted to module
    - import module eg- const api  = require('./api);
    - export module eg- module.export = {}

--- exercises/modules/node/app.js
+++ exercises/modules/node/app.js
@@ -1,33 +1,31 @@
-const api = require('./api')
+const api = require('./api');
 
 const showPostsForCurrentUser = (userId, cb) => {
-  api.getPostsForUser(userId, posts => {
-    const postTemplates = posts.map(post => {
-      return `
+    api.getPostsForUser(userId, posts => {
+        const postTemplates = posts.map(post => {
+            return `
       <div class="post">
         ${post.title}
         ${post.body}
         ${post.createdBy}
       </div>`
+        })
+        cb(postTemplates)
     })
-    cb(postTemplates)
-  })
 }
 
 const showUserProfile = (userId, cb) => {
-  api.getUserById(userId, user => {
-    const profile = `
+    api.getUserById(userId, user => {
+        const profile = `
       <div>
         ${user.name}
       </div>
     `
-    cb(profile)
-  })
+        cb(user)
+    })
 }
 
 module.exports = {
-  showPostsForCurrentUser,
-  showUserProfile
+    showPostsForCurrentUser,
+    showUserProfile
 }
-
-
